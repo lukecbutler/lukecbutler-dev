2e738cfc73fdc3508014e4b749446eee397c8151
tarheelfan200 <tarheelfan3951@gmail.com>
2024-11-24 19:13:38 -0500

    push dev site

--- app.py
+++ app.py
@@ -1,32 +1,21 @@
 # flask app where users can add, view, and delete contacts
 
 from flask import Flask, render_template, request
-import pymysql
+import sqlite3
 
 app = Flask(__name__)
 
 contacts = []
 
-'''
-DB_CONFIG = {
-    "host": "tarheelfan2002.mysql.pythonanywhere-services.com",
-    "user": "tarheelfan2002",
-    "password": "sqlpassword",
-    "database": "tarheelfan2002$Pokedex"
-}
+#connect with database
+def pokemon_db_connection():
+    conn = sqlite3.connect('pokedex.db')
+    #import datbase as a dictionary
+    conn.row_factory = sqlite3.Row
+    return conn
 
 
-'''
-DB_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "password",
-    "database": "Pokemon"
-}
 
-
-def get_db_connection():
-    return pymysql.connect(**DB_CONFIG)
 
 @app.route("/")
 def home():
@@ -41,6 +30,65 @@ def projects():
     return render_template("projects.html")
 
 
+
+###################################################################################
+'''Pokedex'''
+###################################################################################
+
+
+
+@app.route("/pokedex", methods=["GET", "POST"])
+def pokedex():
+    # if statement gets hit when form is submitted
+    if request.method == "POST":
+
+        # start connection with database
+        conn = pokemon_db_connection()
+
+        # receive pokemon ID from form
+        pokemonID = request.form.get("pokemon-id")
+
+        # if user has not entered pokemonID pass error they need to do so
+        if not pokemonID:
+            return render_template("pokedex.html", error = "Please enter a Pokemon ID.")
+
+
+        # try to get the id number of pokemon
+        try:
+            pokemon = conn.execute('SELECT master_id, species, type1, type2, feet, inches, weight, pokedex_entry from pokemon WHERE master_id = ?', (pokemonID,)).fetchone()
+
+        # catches inproper input
+        except (sqlite3.OperationalError, sqlite3.ProgrammingError):
+            return render_template("pokedex.html", error = "Pokemon must be within 1-151")
+        
+        if not pokemon:
+            return render_template("pokedex.html", error = "Pokemon must be within 1-151")
+
+
+        # acts as variables to pass in - since pokemon is coming through as a list we index the info out
+        id, species, type1, type2 = pokemon[0], pokemon[1], pokemon[2], pokemon[3]
+        feet, inches, weight, entry = pokemon[4], pokemon[5], pokemon[6], pokemon[7]
+
+        
+        # special cases to account for
+        if type2 != None:
+            type2 = type2.capitalize()
+        
+        if id == 122:
+            species = "Mr. Mime"
+        else:
+            species = species.capitalize()
+
+        conn.close()
+        return render_template('pokedex.html', pokemon=pokemon,
+                               id = id, species = species, type1 = type1.capitalize(), type2 = type2,
+                                feet = feet, inches = inches, weight = weight, entry = entry
+                               )
+
+
+    # GET - returns blank page as user has not entered pokemon yet
+    return render_template('pokedex.html')
+ 
 
 
 ####################################################################################
